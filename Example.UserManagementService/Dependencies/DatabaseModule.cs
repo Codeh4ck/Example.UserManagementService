d563d272396c57292a232e2178c0daa2038700fc
Nikolas <nickandreou94@gmail.com>
2022-07-20 15:16:38 +0300

    Added DatabaseModule that registers database related IoC dependencies

---
+++ Example.UserManagementService/Dependencies/DatabaseModule.cs
@@ -0,0 +1,27 @@
+﻿using ServiceStack;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using Codelux.Configuration;
+using Codelux.ServiceStack.Utilities;
+using Example.UserManagementService.Internal.DataAccess;
+
+namespace Example.UserManagementService.Dependencies
+{
+    public class DatabaseModule : DependencyModuleBase
+    {
+        public DatabaseModule(ServiceStackHost appHost) : base(appHost) { }
+
+        public override void RegisterDependencies()
+        {
+            IConfigSource configSource = AppHost.Container.Resolve<IConfigSource>();
+            configSource.TryGetString("dbconnectionstring", out string connectionString);
+
+            SqlServerDialect.Provider.GetStringConverter().UseUnicode = true;
+            IDbConnectionFactory dbConnectionFactory = new OrmLiteConnectionFactory(connectionString, SqlServerDialect.Provider);
+
+            AppHost.Register<IDbConnectionFactory>(dbConnectionFactory);
+
+            AppHost.Container.RegisterAutoWiredAs<UserRepository, IUserRepository>();
+        }
+    }
+}
