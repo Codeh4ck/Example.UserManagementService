3333db82aafccad4a6673b499e8ae9663c751566
Nikolas <nickandreou94@gmail.com>
2022-07-20 15:15:04 +0300

    Instantiated ServiceStack plugins on AppConfigurator and added to code to register dependencies on the global container

--- Example.UserManagementService/AppConfigurator.cs
+++ Example.UserManagementService/AppConfigurator.cs
@@ -1,16 +1,34 @@
-﻿using Codelux.ServiceStack;
+﻿using ServiceStack;
+using ServiceStack.Validation;
+using Codelux.Logging;
+using Codelux.ServiceStack;
+using Codelux.ServiceStack.Plugins;
 using Example.UserManagementService.Common;
-using Microsoft.Extensions.Configuration;
-using ServiceStack;
+using Example.UserManagementService.Dependencies;
+using DebugLogger = Codelux.Logging.DebugLogger;
 
 namespace Example.UserManagementService
 {
     public class AppConfigurator : CoreAppConfiguratorBase
     {
-        public AppConfigurator(ServiceStackHost appHost, ConfigurationManager configManager, string environment) 
-            : base(ServiceConstants.ServiceName, appHost)
+        public AppConfigurator(ServiceStackHost appHost, string environment) : base(ServiceConstants.ServiceName, appHost)
         {
+            appHost.Plugins.Add(new ValidationFeature());
+            appHost.Plugins.Add(new MetadataFeature());
+            appHost.Plugins.Add(new OrmLiteMappingFeature());
+            appHost.Plugins.Add(new CoreServiceFeature());
+            appHost.Plugins.Add(new CorsFeature());
+            appHost.Plugins.Add(new RouteFeature());
 
+            ILogger logger = new DebugLogger();
+            appHost.Container.Register<ILogger>(logger);
+
+            logger.LogEvent(LogType.Info, "Service is starting up.");
+
+            new UtilitiesModule(appHost).RegisterDependencies();
+            new DatabaseModule(appHost).RegisterDependencies();
+
+            appHost.Container.RegisterValidators(typeof(AppConfigurator).Assembly);
         }
     }
 }
