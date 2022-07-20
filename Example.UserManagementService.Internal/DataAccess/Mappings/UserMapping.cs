0ecc07a4bceb9c420ec32f5773f9895b48f76459
Nikolas <nickandreou94@gmail.com>
2022-07-20 15:18:19 +0300

    Mapping User model properties to db table columns

---
+++ Example.UserManagementService.Internal/DataAccess/Mappings/UserMapping.cs
@@ -0,0 +1,21 @@
+﻿using Codelux.ServiceStack.OrmLite;
+using Example.UserManagementService.Common.Model;
+
+namespace Example.UserManagementService.Internal.DataAccess.Mappings
+{
+    public class UserMapping : OrmLiteMapping<User>
+    {
+        public UserMapping()
+        {
+            MapToSchema("dbo");
+            MapToTable("users");
+
+            MapToColumn(x => x.Id, "id");
+            MapToColumn(x => x.Username, "id");
+            MapToColumn(x => x.Password, "id");
+            MapToColumn(x => x.Email, "id");
+            MapToColumn(x => x.CreatedAt, "id");
+            MapToColumn(x => x.UpdatedAt, "id");
+        }
+    }
+}
