4f0d4bc77fc7fb7dba89fa0bb2ce6a68b295bcba
Nikolas <nickandreou94@gmail.com>
2022-07-20 15:17:04 +0300

    Added UtilitiesModule that registers helper class IoC dependencies

---
+++ Example.UserManagementService/Dependencies/UtilitiesModule.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack;
+using Codelux.Utilities;
+using Codelux.Utilities.Crypto;
+using Codelux.ServiceStack.Utilities;
+
+namespace Example.UserManagementService.Dependencies
+{
+    public class UtilitiesModule : DependencyModuleBase
+    {
+        public UtilitiesModule(ServiceStackHost appHost) : base(appHost) { }
+
+        public override void RegisterDependencies()
+        {
+            AppHost.Container.RegisterAutoWiredAs<ClockService, IClockService>();
+            AppHost.Container.RegisterAutoWiredAs<Md5PasswordEncryptor, IPasswordEncryptor>();
+        }
+    }
+}
