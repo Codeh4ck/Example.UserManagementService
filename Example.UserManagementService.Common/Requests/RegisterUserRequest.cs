ca9df684e702cbddce53c4360d571499c48c35f5
Nikolas <nickandreou94@gmail.com>
2022-07-20 15:17:22 +0300

    Created register user request and response models

---
+++ Example.UserManagementService.Common/Requests/RegisterUserRequest.cs
@@ -0,0 +1,11 @@
+﻿using Codelux.Common.Requests;
+
+namespace Example.UserManagementService.Common.Requests
+{
+    public class RegisterUserRequest : Request
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Example.UserManagementService.Common/Responses/RegisterUserResponse.cs
@@ -0,0 +1,10 @@
+﻿using Example.UserManagementService.Common.Model;
+
+namespace Example.UserManagementService.Common.Responses
+{
+    public class RegisterUserResponse
+    {
+        public Guid Id { get; set; }
+        public RegisterUserResult Result { get; set; }
+    }
+}
