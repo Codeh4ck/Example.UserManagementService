dc7533faf3cfff04355da89657ade4c71f742fb8
Nikolas <nickandreou94@gmail.com>
2022-07-20 15:10:41 +0300

    Created RouteFeature class to define all REST routes

---
+++ Example.UserManagementService/RouteFeature.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStack;
+
+namespace Example.UserManagementService
+{
+    public class RouteFeature : IPlugin
+    {
+        public void Register(IAppHost appHost)
+        {
+            
+        }
+    }
+}
