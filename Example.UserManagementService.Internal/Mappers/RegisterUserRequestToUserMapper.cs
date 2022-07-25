4d290df96c7d19d45deaa94a979dca4f5dd2f4dd
Nikolas <nickandreou94@gmail.com>
2022-07-25 04:14:44 +0300

    Created mapper that creates a User model out of a RegisterUserRequest model

---
+++ Example.UserManagementService.Internal/Mappers/RegisterUserRequestToUserMapper.cs
@@ -0,0 +1,37 @@
+﻿using Codelux.Mappers;
+using Codelux.Utilities;
+using Codelux.Utilities.Crypto;
+using Codelux.Common.Extensions;
+using Example.UserManagementService.Common.Models;
+using Example.UserManagementService.Common.Requests;
+
+namespace Example.UserManagementService.Internal.Mappers
+{
+    public class RegisterUserRequestToUserMapper : MapperBase<RegisterUserRequest, User>
+    {
+        private readonly IClockService _clockService;
+        private readonly IPasswordEncryptor _passwordEncryptor;
+
+        public RegisterUserRequestToUserMapper(IClockService clockService, IPasswordEncryptor passwordEncryptor)
+        {
+            clockService.Guard(nameof(clockService));
+            passwordEncryptor.Guard(nameof(passwordEncryptor));
+
+            _clockService = clockService;
+            _passwordEncryptor = passwordEncryptor;
+        }
+
+        public override User Map(RegisterUserRequest model)
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                Username = model.Username,
+                Password = _passwordEncryptor.Encrypt(model.Password),
+                Email = model.Email,
+                CreatedAt = _clockService.Now(),
+                UpdatedAt = null
+            };
+        }
+    }
+}
