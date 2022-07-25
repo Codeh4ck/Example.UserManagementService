0c60a438c83696582a1a3b96edfa1efe6c1b2c17
Nikolas <nickandreou94@gmail.com>
2022-07-25 04:23:45 +0300

    Created BasicUser model to represent a user without sensitive data

---
+++ Example.UserManagementService.Common/Models/BasicUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Example.UserManagementService.Common.Models
+{
+    public class BasicUser
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
