64c7ab07e52afe79ed30e1bc84a483ce2884e653
Nikolas <nickandreou94@gmail.com>
2022-07-25 04:15:01 +0300

    Registered mappers and executors to the IoC container

---
+++ Example.UserManagementService/Dependencies/ExecutorsModule.cs
@@ -0,0 +1,22 @@
+﻿using ServiceStack;
+using Codelux.ServiceStack.Utilities;
+using Example.UserManagementService.Executors.RegisterUserExecutor;
+using Example.UserManagementService.Executors.AuthenticateUserExecutor;
+using Example.UserManagementService.Executors.UpdateUserEmailExecutor;
+using Example.UserManagementService.Executors.UpdateUserPasswordExecutor;
+
+namespace Example.UserManagementService.Dependencies
+{
+    public class ExecutorsModule : DependencyModuleBase
+    {
+        public ExecutorsModule(ServiceStackHost appHost) : base(appHost) { }
+
+        public override void RegisterDependencies()
+        {
+            AppHost.Container.RegisterAutoWiredAs<RegisterUserExecutor, IRegisterUserExecutor>();
+            AppHost.Container.RegisterAutoWiredAs<AuthenticateUserExecutor, IAuthenticateUserExecutor>();
+            AppHost.Container.RegisterAutoWiredAs<UpdateUserEmailExecutor, IUpdateUserEmailExecutor>();
+            AppHost.Container.RegisterAutoWiredAs<UpdateUserPasswordExecutor, IUpdateUserPasswordExecutor>();
+        }
+    }
+}
---
+++ Example.UserManagementService/Dependencies/MappersModule.cs
@@ -0,0 +1,19 @@
+﻿using ServiceStack;
+using Codelux.Mappers;
+using Codelux.ServiceStack.Utilities;
+using Example.UserManagementService.Common.Models;
+using Example.UserManagementService.Common.Requests;
+using Example.UserManagementService.Internal.Mappers;
+
+namespace Example.UserManagementService.Dependencies
+{
+    public class MappersModule : DependencyModuleBase
+    {
+        public MappersModule(ServiceStackHost appHost) : base(appHost) { }
+
+        public override void RegisterDependencies()
+        {
+            AppHost.Container.RegisterAutoWiredAs<RegisterUserRequestToUserMapper, IMapper<RegisterUserRequest, User>>();
+        }
+    }
+}
