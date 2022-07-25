ce11cb902146fc98a0278efc888f95e73b6cdceb
Nikolas <nickandreou94@gmail.com>
2022-07-25 04:05:32 +0300

    Defined service routes

--- Example.UserManagementService/RouteFeature.cs
+++ Example.UserManagementService/RouteFeature.cs
@@ -1,4 +1,5 @@
 ﻿using ServiceStack;
+using Example.UserManagementService.Common.Requests;
 
 namespace Example.UserManagementService
 {
@@ -6,7 +7,10 @@ namespace Example.UserManagementService
     {
         public void Register(IAppHost appHost)
         {
-            
+            appHost.Routes.Add<RegisterUserRequest>("/api/users", ApplyTo.Post);
+            appHost.Routes.Add<AuthenticateUserRequest>("/api/users", ApplyTo.Get);
+            appHost.Routes.Add<UpdateUserEmailRequest>("/api/users/email", ApplyTo.Put);
+            appHost.Routes.Add<UpdateUserPasswordRequest>("/api/users/password", ApplyTo.Put);
         }
     }
 }
